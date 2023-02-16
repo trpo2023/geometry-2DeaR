9209420b7a37a99537dc3ccbe2945564ac442d7d
Maksim <2dearr2@gmail.com>
2023-02-16 12:09:26 +0700

    Edit Makefile and main.c

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int main()
-{
-    printf("Hello, World\n");
+
+int main(int argc, char* argv[])
+{ 
+  FILE *f;
+  char buffer[256];
+  f = fopen(argv[1], "r");
+  while((fgets(buffer, 256, f))!=NULL)
+        {
+            printf("%s", buffer);
+        }
+        printf("\n");
+        fclose(f);
+  
     return 0;
 }
