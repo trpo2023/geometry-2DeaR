48e3bd10945a8f9e8c29804108bbdc013ef82c2a
Maksim <2dearr2@gmail.com>
2023-04-19 14:33:00 +0700

    Edit Makefile for Tests

--- test/parser_test.c
+++ test/parser_test.c
@@ -6,7 +6,7 @@ CTEST(ctest, check_perimetr)
 {
     double pi = 3.14;
     float rad = 2.3;
-    const double expected = 2 * Pi * rad;
+    const double expected = 2 * pi * rad;
     const double result = pcircle(rad);
     ASSERT_DBL_NEAR_TOL(expected, result, 1e-1);
 }
